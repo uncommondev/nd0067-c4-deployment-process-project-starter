b232f8c70abd454f9a4056081cf85ef5a0be9966
Uncommon Dev <uncommondev344@gmail.com>
2021-12-31 23:26:54 +0000

    Fix For Tests
    Another reported fix for the issue

--- udagram-frontend/src/karma.conf.js
+++ udagram-frontend/src/karma.conf.js
@@ -27,10 +27,10 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     customLaunchers: {
-      Chrome_no_sandbox: {
-        base: 'Chrome',
-        flags: ['--no-sandbox'] // for running within Docker
-      }
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+    }
     },
     singleRun: false
   });

74abf6be3b734f84dad30b298cd4bc228098dbfe
Uncommon Dev <uncommondev344@gmail.com>
2022-01-01 15:04:24 +0000

    Enabled pre-flight CORS
    Attempt to fix the CORS issue

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -24,6 +24,8 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   app.use(cors());
 
+  app.options('*', cors())
+
   app.use("/api/v0/", IndexRouter);
 
   // Root URI call

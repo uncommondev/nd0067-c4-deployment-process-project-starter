8d068ab6dae29d3d022fac141bd865b046380263
Uncommon Dev <uncommondev344@gmail.com>
2022-01-01 10:50:13 +0000

    Updated Frontend Deployment Script
    Updated the build path, it's "www" instead of "build"

--- deploy/frontend.sh
+++ deploy/frontend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Frontend Deployment Activated"
-aws s3 cp --recursive ../udagram-frontend/build s3://udagram-ta-udacity/
+aws s3 cp --recursive ../udagram-frontend/www s3://udagram-ta-udacity/
